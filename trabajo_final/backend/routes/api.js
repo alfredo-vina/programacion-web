5fbe5207c72c0d547c3c78e8ad9961c307f3724e
alfredo-vina <56701091+alfredo-vina@users.noreply.github.com>
2024-08-28 03:08:14 -0300

    api de envio de mail

--- trabajo_final/backend/routes/api.js
+++ trabajo_final/backend/routes/api.js
@@ -23,4 +23,29 @@ router.get('/libros', async function (req, res, next) {
     res.json(libros);
 })
 
+router.post('/contacto', async (req, res) => {
+    const mail = {
+        to: 'alfred@montevideo.com.uy',
+        subject: 'Contacto',
+        html: req.body.nombre + " se contacta y espera respuesta en " + req.body.email +
+        "<br/>Mensaje: <br/>" + req.body.mensaje
+    }
+})
+
+const transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth:{
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    }
+});
+
+await transport.sendMail(mail);
+
+res.status(201).json({
+    error:false, 
+    message: "Mensaje enviado"
+});
+
 module.exports = router;
