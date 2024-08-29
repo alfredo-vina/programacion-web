1401273dfc72570d8b569a1581769b971e6268ae
alfredo-vina <56701091+alfredo-vina@users.noreply.github.com>
2024-08-29 17:50:05 -0300

    export de BD sql

--- trabajo_final/sql/trabajo_practico.sql
+++ trabajo_final/sql/trabajo_practico.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-08-2024 a las 22:43:16
+-- Tiempo de generación: 29-08-2024 a las 22:48:52
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -44,6 +44,26 @@ INSERT INTO `libros` (`id`, `titulo`, `subtitulo`, `imagen`, `precio`) VALUES
 (9, 'The Art of Crafting', 'User Stories', 'w2hplt7i5zuvmstmgtgx', 12),
 (11, 'Python para Principiantes', '', 'kcotsxwtn3x52bvo7gzw', 13);
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `usuario`, `password`) VALUES
+(1, 'alfredo', '93279e3308bdbbeed946fc965017f67a'),
+(2, 'user', '93279e3308bdbbeed946fc965017f67a');
+
 --
 -- Índices para tablas volcadas
 --
@@ -55,6 +75,12 @@ ALTER TABLE `libros`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -63,6 +89,12 @@ ALTER TABLE `libros`
 --
 ALTER TABLE `libros`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
